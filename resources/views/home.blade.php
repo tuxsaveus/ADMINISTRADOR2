81673d52a428457a3619171b82a8fa08ae632154
tuxsaveus <tuxsaveus@gmail.com>
2020-09-17 09:36:40 -0500

    Subida de proyecto

---
+++ resources/views/home.blade.php
@@ -0,0 +1,72 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Tablero</h1>
+@stop
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            <h1 class="card-title">Hola mundo</h1>
+        </div>
+        <div class="card-body">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, voluptatum nostrum nulla ducimus temporibus libero. Nihil a rem, eos aliquid in, facere doloribus quos accusantium doloremque molestias sunt tempora officiis!</p>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            <h1 class="card-title">Hola mundo</h1>
+        </div>
+        <div class="card-body">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, voluptatum nostrum nulla ducimus temporibus libero. Nihil a rem, eos aliquid in, facere doloribus quos accusantium doloremque molestias sunt tempora officiis!</p>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            <h1 class="card-title">Hola mundo</h1>
+        </div>
+        <div class="card-body">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, voluptatum nostrum nulla ducimus temporibus libero. Nihil a rem, eos aliquid in, facere doloribus quos accusantium doloremque molestias sunt tempora officiis!</p>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            <h1 class="card-title">Hola mundo</h1>
+        </div>
+        <div class="card-body">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, voluptatum nostrum nulla ducimus temporibus libero. Nihil a rem, eos aliquid in, facere doloribus quos accusantium doloremque molestias sunt tempora officiis!</p>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            <h1 class="card-title">Hola mundo</h1>
+        </div>
+        <div class="card-body">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, voluptatum nostrum nulla ducimus temporibus libero. Nihil a rem, eos aliquid in, facere doloribus quos accusantium doloremque molestias sunt tempora officiis!</p>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            <h1 class="card-title">Hola mundo</h1>
+        </div>
+        <div class="card-body">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, voluptatum nostrum nulla ducimus temporibus libero. Nihil a rem, eos aliquid in, facere doloribus quos accusantium doloremque molestias sunt tempora officiis!</p>
+        </div>
+    </div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script>
+        Swal.fire(
+            'Good job!',
+            'You clicked the button!',
+            'success'
+        )
+    </script>
+@stop
